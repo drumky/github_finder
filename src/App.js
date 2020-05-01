b361ea599304fc8710fea024e5fdb52e7ca196c7
drumky <axeljcob@gmail.com>
2020-05-01 14:37:08 +0800

    rendered a hard coded users(using data from Github api).

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from react</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar title="Github Finder" icon="fab fa-github" />
+        <Users />
+      </div>
+    );
+  }
 }
 
 export default App;
